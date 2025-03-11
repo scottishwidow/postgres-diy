bbeaa6c7d9356026fddc89b212bd430730015e7e
scottishwidow <mike.voznyanskix@gmail.com>
2025-03-11 14:07:04 +0000

    outputs for bootstrap separated

---
+++ terraform/live/test/bootstrap/outputs.tf
@@ -0,0 +1,7 @@
+output "terraform_state_bucket_name" {
+  value = module.bootstrap.terraform_state_bucket_domain_name
+}
+
+output "terraform_state_lock" {
+  value = module.bootstrap.terraform_state_lock_name
+}
